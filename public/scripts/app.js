418fa55551aac406ee763208214d610ba62973a2
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2015-03-08 09:02:58 +0800

    localstorage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
-angular.module('todos', [])
-    .factory('storage', function(){
+angular.module('todos', ['LocalStorageModule'])
+    .factory('arraystorage', function(){
         var todos = [
             {
                 done: false,
@@ -45,6 +45,60 @@ angular.module('todos', [])
         };
         return functions;
     })
+    .factory('localstorage', ['localStorageService', function(localStorageService){
+        var todos = localStorageService.get('todos') || [];
+        var functions = {
+            commit: function() {
+                localStorageService.set('todos', todos);
+            },
+            all: function() {
+                return todos;
+            },
+            create: function(todo) {
+                todos.push(todo);
+                this.commit();
+            },
+            remove: function(todo) {
+                var index = todos.indexOf(todo);
+                todos.splice(index, 1);
+                this.commit();
+            },
+            update: function(todo) {
+                todo.title = todo.editingTitle;
+                this.commit();
+            },
+            clearDone: function() {
+                todos = this.getActiveTodos();
+                this.commit();
+            },
+            getActiveTodos: function() {
+                return todos.filter(function(todo){
+                    return !todo.done;
+                });
+            },
+            getCompletedTodos: function() {
+                return todos.filter(function(todo){
+                    return !todo.done;
+                });
+            },
+            getDoneTodos: function() {
+                return todos.filter(function(todo){
+                    return todo.done;
+                });
+            }
+        };
+        return functions;
+    }])
+    .factory('storage', [
+        'arraystorage', 
+        'localstorage', 
+        function(
+            arraystorage, 
+            localstorage
+        ){
+            return localstorage;
+        }
+    ])
     .controller('TodoController', [
         '$scope', 
         'storage', 
@@ -70,13 +124,13 @@ angular.module('todos', [])
                     case 13:
                         if(!todo && this.newTodoTitle) {
                             storage.create({title: this.newTodoTitle, done: false});
-                        } else {
+                        } else if(todo) {
                             storage.update(todo);
                         }
                     case 27:
                         if(!todo) {
                             this.newTodoTitle = '';
-                        } else {
+                        } else if(todo) {
                             todo.editingTitle = '';
                             todo.editing = false;
                         }
@@ -87,11 +141,16 @@ angular.module('todos', [])
                 todo.editingTitle = todo.title;
                 todo.editing = true;
             },
-            deleteTodo : storage.remove,
+            deleteTodo : function() {
+                storage.remove();
+            },
             getIncompleted : storage.getCompletedTodos,
             clearDone : function(){
                 storage.clearDone();
                 this.loadTodos();
+            },
+            commitTodo: function() {
+                storage.commit();
             }
         });
 
